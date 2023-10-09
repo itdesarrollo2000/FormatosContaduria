3cd218f3847226d894723db64db7122b45324ab1
Juan Carlos <JuanCarlos.CorreaV@gmail.com>
2023-10-09 14:02:59 -0600

    correción rutas path home

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -5,7 +5,7 @@ import WebPage from "../Pages/WebPage";
 function AuthRoutes () {
     return (
         <Routes>
-            <Route path="home" element={<WebPage/>}/>
+            <Route path="home/*" element={<WebPage/>}/>
 
             <Route path="/" element={<Navigate to="/auth/home"/>}/>
         </Routes>
