405f2c284a2ebb21add0920a0937d0f5e02a8cd7
Juan Carlos <JuanCarlos.CorreaV@gmail.com>
2023-10-09 13:59:08 -0600

    Correción del main

--- src/auth/Pages/WebPage.jsx
+++ src/auth/Pages/WebPage.jsx
@@ -1,12 +1,13 @@
-
+import AppRouter from "../../components/Routes/AppRouter";
 import JournalLayout from "./JournalLayout";
 
 
 
 const WebPage = () => {
   return (
+    
     <JournalLayout>
-      
+      <AppRouter/>
     </JournalLayout>
   );
 };
--- src/auth/Pages/index.js
+++ src/auth/Pages/index.js
@@ -1,1 +1,2 @@
 export * from "./WebPage"
+export * from "./AgregarEMP"
