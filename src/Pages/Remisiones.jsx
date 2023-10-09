27793fab2a097d6945699ce95f03cd0fd960c088
IsabelinoCadena <isabelinocadena@gmail.com>
2023-10-09 13:47:11 -0600

    Se agrega paguina de remisiones con tabla

---
+++ src/Pages/Remisiones.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Space, Table, Tag } from "antd";
+const columns = [
+  {
+    title: "Cantidad",
+    dataIndex: "Cantidad",
+    key: "Cantidad",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Unidad",
+    dataIndex: "Unidad",
+    key: "Unidad",
+  },
+  {
+    title: "Concepto",
+    dataIndex: "Concepto",
+    key: "Concepto",
+  },
+  {
+    title: "Precio unitario",
+    dataIndex: "Precio unitario",
+    key: "Precio unitario",
+  },
+  {
+    title: "Importe",
+    dataIndex: "Importe",
+    key: "Importe",
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
+const App = () => <Table columns={columns} dataSource={data} bordered />;
+export default App;
